ead1c9d8b2945fabcba5c863237f692609323fd7
Eduardo Berlatto <eduardoberlatto@gmail.com>
2025-06-13 12:16:33 -0300

    aula 6(final): ajusta detalhes finais e conclui projeto

--- src/br/com/dio/ui/custom/input/NumberText.java
+++ src/br/com/dio/ui/custom/input/NumberText.java
@@ -1,15 +1,20 @@
 package br.com.dio.ui.custom.input;
 
 import br.com.dio.model.Space;
+import br.com.dio.service.EventEnum;
+import br.com.dio.service.EventListener;
 
-import javax.swing.*;
+import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import static br.com.dio.service.EventEnum.CLEAR_SPACE;
 import static java.awt.Font.PLAIN;
 
-public class NumberText extends JTextField {
+public class NumberText extends JTextField implements EventListener {
+
     private final Space space;
 
     public NumberText(final Space space) {
@@ -22,23 +27,23 @@ public class NumberText extends JTextField {
         this.setHorizontalAlignment(CENTER);
         this.setDocument(new NumberTextLimit());
         this.setEnabled(!space.isFixed());
-        if (space.isFixed()) {
+        if (space.isFixed()){
             this.setText(space.getActual().toString());
         }
         this.getDocument().addDocumentListener(new DocumentListener() {
 
             @Override
-            public void insertUpdate(DocumentEvent e) {
+            public void insertUpdate(final DocumentEvent e) {
                 changeSpace();
             }
 
             @Override
-            public void removeUpdate(DocumentEvent e) {
+            public void removeUpdate(final DocumentEvent e) {
                 changeSpace();
             }
 
             @Override
-            public void changedUpdate(DocumentEvent e) {
+            public void changedUpdate(final DocumentEvent e) {
                 changeSpace();
             }
 
@@ -51,5 +56,12 @@ public class NumberText extends JTextField {
             }
 
         });
+    }
+
+    @Override
+    public void update(final EventEnum eventType) {
+        if (eventType.equals(CLEAR_SPACE) && (this.isEnabled())){
+            this.setText("");
+        }
     }
 }
